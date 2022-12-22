d26f2fb7b0c9f783ec7dc135956a34dfa5253524
Ali <ali.ozgur@example.com>
2022-12-22 15:12:05 +0300

    edit login component

--- src/components/globalStyles/Flex.style.jsx
+++ src/components/globalStyles/Flex.style.jsx
@@ -1,13 +1,12 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 //styled-components yapısı ile her yerde kullanabileceğimiz ortalama amaçlı bir component oluşturdum.
 export const Flex = styled.div`
-display: flex;
-justify-content:${({justify})=>(justify ? justify: "center")};
-align-items: ${({align})=> (align ? align: "center")};
-flex-wrap:${({wrap})=> ( wrap && wrap)};
-
-
+  display: flex;
+   
+    /*örnegin propsta attirube olarak space-between varsa onu al yoksa justify-center'i al demiş oluyorum*/
+  
+  justify-content: ${({ justify }) => (justify ? justify : "center")};
+  align-items: ${({ align }) => (align ? align : "center")};
+  flex-wrap: ${({ wrap }) => wrap && wrap};
 `;
-

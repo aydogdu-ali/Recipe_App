a797a8137531e043e12b8bb66a968c444d95a50b
Ali <ali.ozgur@example.com>
2022-12-23 14:57:04 +0300

    edit navbar component and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import AppRouter from "./router/AppRouter"
+import { ThemeProvider } from 'styled-components';
+import theme from "./components/globalStyles/theme"
 
 function App() {
   return (
     <div className="App">
       {/*App da fazla dosya görünmemesi için AppRouter Componentini buraya çağırdım*/}
-<AppRouter/>
+      <ThemeProvider theme={theme}>
+        <AppRouter />
+      </ThemeProvider>
+
     </div>
   );
 }
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import {Nav, Brand} from "./Navbar.style"
+import {Nav, Brand, Menu, MenuLink,Hamburger} from "./Navbar.style"
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Navbar = () => {
+
+// Hamburger Menü için toogle işlemi tanımladım.
+
+const [isOpen, setIsOpen] = useState(false)
+
+
   return (
     <Nav justify= "space-between" wrap= "wrap">
       <Brand to="/">
         <i>{"<Chef>"  } Recipe</i>
        </Brand> 
 
-<div>
-  <Link to="/" > Home</Link> 
-  <Link to="about" > About</Link> 
-  <Link to="register" > Register</Link> 
-  <Link to="Logout" > Logout</Link> 
-</div>
+
+<Hamburger onClick = {()=>setIsOpen(!isOpen)}>
+<GiHamburgerMenu />
+</Hamburger>
+
+
+<Menu isOpen ={isOpen} onClick ={()=>setIsOpen(false)}>
+  <MenuLink to="/" > Home</MenuLink> 
+  <MenuLink to="about" > About</MenuLink> 
+  <MenuLink to="register" > Register</MenuLink> 
+  <MenuLink to="Logout" onClick={()=>sessionStorage.clear()}> Logout</MenuLink> 
+</Menu>
 
     </Nav>
   )

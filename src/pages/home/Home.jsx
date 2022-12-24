711fcb3123acc83d39110574e29537af9aa8c669
Ali <ali.ozgur@example.com>
2022-12-24 22:37:29 +0300

    data fetchings at home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
+  const APP_ID= "56c66d94";
+  const APP_KEY ="b748a0cefd89b21ec128cd38783d9530";
+  const [query, setQuery] = useState("egg")
+  const [selectedMeal, setSelectedMeal] = useState("breakfast");
+  const [recipes, setRecipes] = useState(null);
+  // const mealType = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
+
+  const getData= async()=>{
+    const {data} = await axios(url)
+    // console.log(data)
+    setRecipes(data.hits);
+   
+  }
+  console.log(recipes)
+
+useEffect(() => {
+  getData()
+ 
+}, [])
+
+
   return (
     <div>
       Home
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // Kullanıcı bilgisini locale yazdım.
     sessionStorage.setItem("user",JSON.stringify(userInfo))
     navigate(-1)
   }
